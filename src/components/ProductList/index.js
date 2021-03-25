bf9c1b83463891047841498398b65af7f0e1c265
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-03-25 23:42:56 +0200

    Додає fetch бази данних

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -2,6 +2,13 @@ import ProductItem from "../ProductItem";
 import products from "../../data/products.json";
 
 const ProductList = () => {
+  const apiKey = "hZxMaekVwU4tKeBajxXTPFzSBRyMYGNT";
+  fetch(
+    `https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then(({ results }) => console.log(results));
+
   return (
     <ul className="d-flex flex-wrap pl-0">
       {products.map(({ id, title, photo, amount, price }) => (
